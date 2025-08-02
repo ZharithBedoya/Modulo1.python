9bc38b429d529db936cb9959726b08d996cf45c0
ZharithBedoya <ZharithBedoya>
2025-08-02 13:59:43 -0500

    ejercicio de adivinar numero

--- adivinaelnum.py
+++ adivinaelnum.py
@@ -4,3 +4,11 @@ debe adivinarlo. En cada intento, el programa le dirá si el número ingresado e
 mayor o menor que el número secreto. El juego termina cuando el usuario adivina
 el número."""
 
+num=int(input("Ingrese un numero entre 1 y 100: "))
+num_secreto= 67
+
+if num>num_secreto:
+    print(f"El numero secreto es menor")
+elif num<num_secreto:
+    print(f"El numero secreto es mayor")
+
