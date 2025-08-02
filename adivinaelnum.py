a1ebf45b20e842b9ea548f4183197f794c7dac52
ZharithBedoya <ZharithBedoya>
2025-08-02 13:42:05 -0500

    ejercicio de adivinar numero

---
+++ adivinaelnum.py
@@ -0,0 +1,6 @@
+"""Crea un juego donde el programa "piensa" un número secreto entre 1 y 100
+(puedes definirlo estáticamente, por ejemplo, numero_secreto = 42). El usuario
+debe adivinarlo. En cada intento, el programa le dirá si el número ingresado es
+mayor o menor que el número secreto. El juego termina cuando el usuario adivina
+el número."""
+
